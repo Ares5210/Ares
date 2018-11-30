83feb79c8da4f48aeeb721a8321c9ae2346ce0ac
Ares <ares_5210@163.com>
2018-11-30 08:38:27 +0000

    简单的静态页面完成

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ares App - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+    <div class="container">
+		<div class="col-md-offset-1 col-md-10">
+			@yield('content')
+			<div class="col-md-12">
+				@include('layouts._footer')
+			</div>      	
+		</div>
+    </div>
+  </body>
+</html>
